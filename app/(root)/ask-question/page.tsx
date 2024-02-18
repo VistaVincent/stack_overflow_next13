4a07be1de4ac049bfd45dee1ec5f0814b2b5efb4
Vista Vincent <4vista4vincent@gmail.com>
2024-02-18 13:20:27 +0530

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { UserButton } from "@clerk/nextjs";
+
+const AskQuestion = () => {
+  return (
+    <>
+    <UserButton />
+    <div>Ask Question</div>
+    </>
+    
+  )
+}
+
+export default AskQuestion
