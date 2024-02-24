dcf24c6505f57e05f6bd93a5f5270347bf2e5120
Vista Vincent <4vista4vincent@gmail.com>
2024-02-24 12:30:42 +0530

    added Theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from "next/font/google";
 import  React  from 'react';
 import "./globals.css";
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 export const metadata: MetaData = {
   title: 'DevFlow',
@@ -30,18 +31,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 'primary-text-gradient hover:text-primary-500'
-      }
-    }}>
+    
       <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}</body>
+        <ClerkProvider
+            appearance={{
+              elements: {
+                formButtonPrimary: 'primary-gradient',
+                footerActionLink: 'primary-text-gradient hover:text-primary-500'
+              }
+            }}>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+        </body>
       </html>
-    </ClerkProvider>
+    
     
   );
 }
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+
+interface ThemeContextType{
+    mode: string,
+    setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({children}: {children: React.ReactNode}){
+    const [mode, setMode] = useState('');
+
+    const handleThemeChange = () => {
+        if(mode === 'dark'){
+            setMode('light');
+            document.documentElement.classList.add('light');  // used to add class names to the root (html tag) 
+        }else{
+            setMode('dark');
+            document.documentElement.classList.add('dark');        
+        }
+    }
+
+    useEffect(() => {
+        handleThemeChange();
+    },[mode]);
+
+    return(
+        <ThemeContext.Provider value = {{mode,setMode}}>
+            {children}
+        </ThemeContext.Provider>
+    ) 
+}
+
+export function useTheme(){
+    const context = useContext(ThemeContext);
+
+    if(context === undefined){
+        throw new Error('useTheme must be used within the Theme Provider');
+    
+    } 
+    return context;
+}
