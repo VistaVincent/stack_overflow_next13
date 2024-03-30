b97610eccf896aa0608caa2d3620ce6b82e52604
Vista Vincent <4vista4vincent@gmail.com>
2024-03-30 19:00:48 +0530

    added filter componenet to pages

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { Input } from "@/components/ui/input"
+import HomeFilter from "@/components/shared/search/HomeFilter"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="flex w-full flex-col-reverse gap-4 justify-between sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link className="self-end" href= "/ask-question">
+          <Button className="primary-gradient self-start min-h-[46px] px-4 py-3 text-light-900">Ask a Question</Button>
+        </Link>
+      </div>
+      <HomeFilter/>
+        
+      
+
+    </>
   )
 }
 
---
+++ components/shared/search/Filter.tsx
@@ -0,0 +1,39 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+   
+  interface Props{
+    filters:{
+      name:string,
+      value:string
+    }[],
+    otherClasses:string,
+    containerClasses:string
+  }
+  export function Filter({filters,otherClasses,containerClasses}:Props) {
+    return (
+      <div className={`${containerClasses}`}>
+        <Select>
+          <SelectTrigger className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}>
+            <SelectValue placeholder="Select A Filter" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {filters.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>    
+      </div>
+    )
+  }
